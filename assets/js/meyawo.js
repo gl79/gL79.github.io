f43700dd4c3f9ab2f1aa0bf4d90178b70b115374
Gery Liam <77185808+gl79@users.noreply.github.com>
2025-08-10 00:40:17 +0700

    Update meyawo.js
    new features - august 2025

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -1,16 +1,3 @@
-/*!
-=========================================================
-* Meyawo Landing page
-=========================================================
-
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 
 // smooth scroll
 $(document).ready(function () {
@@ -36,3 +23,79 @@ $('#nav-toggle').click(function () {
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+$(document).ready(function () {
+    // Show or hide scroll to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scrollTopBtn').fadeIn();
+        } else {
+            $('#scrollTopBtn').fadeOut();
+        }
+    });
+
+    // Scroll smoothly to top when button clicked
+    $('#scrollTopBtn').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
+
+document.querySelectorAll('.faq-question').forEach(question => {
+    question.addEventListener('click', () => {
+        const faqItem = question.parentElement;
+
+        // toggle active class
+        faqItem.classList.toggle('active');
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const progressBars = document.querySelectorAll(".progress-fill");
+
+    const options = {
+        threshold: 0.5,
+    };
+
+    function animateProgress(entries, observer) {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const bar = entry.target;
+                const progressValue = bar.getAttribute("data-progress");
+                bar.style.width = progressValue + "%";
+                observer.unobserve(bar);
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver(animateProgress, options);
+
+    progressBars.forEach((bar) => {
+        observer.observe(bar);
+    });
+});
+
+const text = "LET'S GOOO!!";
+const typingSpeed = 120;
+let index = 0;
+
+function typeWriter() {
+    if (index < text.length) {
+        document.getElementById("typing-text").textContent += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, typingSpeed);
+    } else {
+        const loader = document.getElementById("loader");
+        loader.style.opacity = 0;
+        // langsung sembunyikan tanpa delay
+        loader.style.display = "none";
+    }
+}
+
+
+window.onload = () => {
+    typeWriter();
+    setTimeout(() => {
+        document.getElementById("loader").style.display = "none";
+    }, 3000);
+}
